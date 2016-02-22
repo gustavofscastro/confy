cb4146213ef56815b48d5b50a29084b6c0039f7f
gustavofscastro <gustavofscastro@gmail.com>
2016-02-21 19:04:05 -0600

    add database connection

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var stylus = require('stylus');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
@@ -16,7 +17,7 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-app.use(stylus.middleware ( 
+app.use(stylus.middleware( 
   {
     src: __dirname + '/public',
     compile: compile
@@ -25,14 +26,20 @@ app.use(stylus.middleware (
 
 app.use(express.static(__dirname + '/public'));
 
+mongoose.connect('mongodb://confydbuser:confy408525@ds013898.mongolab.com:13898/confydb');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback() {
+    console.log('database opened');
+});
+
 app.get('/partials/:partialPath', function(req, res) {
-    console.log(req.params.partialPath);
-    res.render('/partials/' + req.params.partialPath);
+    res.render('partials/' + req.params.partialPath);
 });
 
 app.get('*', function(req, res) {
     res.render('index');
 });
 
-app.listen(process.env.PORT, process.env.IP);
-console.log('Server listening ' + process.env.PORT + ' ...');
+app.listen(3000);
+console.log('Server listening ' + 3000 + ' ...');
